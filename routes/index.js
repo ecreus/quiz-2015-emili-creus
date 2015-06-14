290a3dbbd8479c08c57fc84f500a29d237d86fb4
ecreus <emili.creus@gmail.com>
2015-06-14 08:06:43 +0200

    Ejercico P2P Modulo 6

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller.js');
 
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'QUIZ' });
@@ -10,5 +11,6 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author',quizController.author);
 
 module.exports = router;
